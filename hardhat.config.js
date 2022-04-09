7a2f2bfa9144829d3e4570d3fb42a06c20fd1e60
MuhammadNabeelNisar <muhammad.nabeel.nisar@gmail.com>
2022-04-09 13:13:38 +0500

    init

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,47 +1,44 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-truffle5");
+require("dotenv").config();
+require("@nomiclabs/hardhat-etherscan");
 
-  // All Requirments to import  
-  require("@nomiclabs/hardhat-waffle");
-  require('hardhat-contract-sizer');
-  require("hardhat-gas-reporter");
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
 
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
 
-  // Main Accounts 
-  task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-    const accounts = await hre.ethers.getSigners(()=>{
-      
-    });
-  
-    for (const account of accounts) {
-      console.log(account.address);
-    }
-  });
-  
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
 
-
-  // Main body module
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
 module.exports = {
-
-  // solidity: "0.8.0",
   solidity: {
     version: "0.8.9",
     settings: {
       optimizer: {
         enabled: true,
-        runs: 200
-      }
-    }
+        runs: 200,
+      },
+    },
   },
-  optimizer: {
-    enabled: true,
-    runs: 200
-},
-contractSizer: {
-  alphaSort: true,
-  runOnCompile: true,
-  disambiguatePaths: false,
-},
-gasReporter: {
-  currency: 'CHF',
-  gasPrice: 21
-}
+  networks: {
+    test: {
+      url: process.env.URL,
+      accounts: [process.env.PVT_KEY],
+    },
+  },
+  localhost: {
+    url: "http://127.0.0.1:7545",
+  },
+  etherscan: {
+    apiKey: process.env.API_KEY,
+  },
 };
